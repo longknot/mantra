6bbed6f877e4398090366cd61351b63e69e187e9
Mattias Andersson <mattias@longknot.com>
2026-06-30 22:57:10 +0200

    Improve notebook example with chart series (vega lite).

--- packages/viz/vega_lite.m
+++ packages/viz/vega_lite.m
@@ -2,6 +2,7 @@ package viz/vega_lite;
 
 mime = "application/vnd.vegalite.v6+json";
 schema = "https://vega.github.io/schema/vega-lite/v6.json";
+__line_row = 1;
 
 // Materializes a basic line-chart specification at SPEC.
 // Callers place row objects under SPEC.data.values[n].
@@ -18,17 +19,38 @@ rule line_xy [
 
 // Materializes y = EXPR over a staged sequence or range driver bound to x.
 rule line_range [
+  line_range SPEC RANGE EXPR SERIES -- => {
+    { viz.vega_lite.line_xy SPEC "x" "y" },
+    { assign_path "" { fmt "%s.encoding.color.field" [ SPEC ] } "series" },
+    { assign_path "" { fmt "%s.encoding.color.type" [ SPEC ] } "nominal" },
+    { viz.vega_lite.line_range_series SPEC RANGE EXPR SERIES }
+  } SPEC,
   line_range SPEC RANGE EXPR -- => {
     { viz.vega_lite.line_xy SPEC "x" "y" },
     { viz.vega_lite.__line_row = 1 },
-    {
-      [ scope {
-          { assign_path { fmt "%s.data.values[%s].x" [ SPEC $viz.vega_lite.__line_row ] } x },
-          { assign_path { fmt "%s.data.values[%s].y" [ SPEC $viz.vega_lite.__line_row ] } { ~ + ( EXPR ) } },
-          { assign_path "viz.vega_lite.__line_row" { ~ + $viz.vega_lite.__line_row 1 } }
-      } ] :: RANGE @ x
-    }
+    { viz.vega_lite.line_range_values SPEC RANGE EXPR }
   } SPEC
+];
+
+rule line_range_values [
+  line_range_values SPEC RANGE EXPR -- => {
+    [ scope {
+        { assign_path { fmt "%s.data.values[%s].x" [ SPEC $viz.vega_lite.__line_row ] } x },
+        { assign_path { fmt "%s.data.values[%s].y" [ SPEC $viz.vega_lite.__line_row ] } { ~ + ( EXPR ) } },
+        { assign_path "viz.vega_lite.__line_row" { ~ + $viz.vega_lite.__line_row 1 } }
+    } ] :: RANGE @ x
+  }
+];
+
+rule line_range_series [
+  line_range_series SPEC RANGE EXPR SERIES -- => {
+    [ scope {
+        { assign_path { fmt "%s.data.values[%s].x" [ SPEC $viz.vega_lite.__line_row ] } x },
+        { assign_path { fmt "%s.data.values[%s].y" [ SPEC $viz.vega_lite.__line_row ] } { ~ + ( EXPR ) } },
+        { assign_path { fmt "%s.data.values[%s].series" [ SPEC $viz.vega_lite.__line_row ] } SERIES },
+        { assign_path "viz.vega_lite.__line_row" { ~ + $viz.vega_lite.__line_row 1 } }
+    } ] :: RANGE @ x
+  }
 ];
 
 rule square_range [
